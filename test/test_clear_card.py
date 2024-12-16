85049f8ec9da1b70e6f5d9503525328a2428a27b
alphaone <bourlleo@gmail.com>
2024-12-16 15:49:18 +0100

    ajout du path
    ajout du path pour lancer les test depuis le dossier

--- test/test_clear_card.py
+++ test/test_clear_card.py
@@ -1,7 +1,11 @@
+import sys
+import os
+
+# Ajouter le chemin du dossier principal au sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
 from cart import Cart
 from product import Product
-
 class TestCart(unittest.TestCase):
 
     def setUp(self):
